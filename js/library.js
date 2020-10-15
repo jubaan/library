7198f8f025f6d09e8711e8b0cf884f750540c9d8
kitpao <kitpao91@gmail.com>
2020-10-15 19:53:04 +0000

    add pseudocode in js for displaying books

--- js/library.js
+++ js/library.js
@@ -12,3 +12,20 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
   return myLibrary;
 }
+
+function checkBooks() {
+  current_book = {}
+  myLibrary.forEach (book => book);
+}
+
+function displayBook() {
+  //will take current book
+  //generate a tr and attach it to table as last child
+  //generate a td for each property value (loop for .. in ..)
+
+}
+
+function displayLibrary() {
+  checkBooks();
+}
+
