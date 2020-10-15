d79b813f5d3f936b47132dfa2934209d15d89266
kitpao <kitpao91@gmail.com>
2020-10-15 21:10:52 +0000

    WIP: Adding functions to display the books in library

--- js/library.js
+++ js/library.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(author = 'anonymous', title, pages = 0, read = false) {
   this.author = author;
@@ -17,7 +17,7 @@ function addBookToLibrary(book) {
 function askUserForBookInfo() {
   let title = prompt("What's the book title?", '');
   let author = prompt("Who's the book's author?", '');
-  let pages = prompt('How many pages does it have?', '');
+  let pages = +prompt('How many pages does it have?', '');
   let read = confirm('Have you read this book? if you have click "OK" if not "Cancel"');
 
   let book = new Book(author, title, pages, read);
@@ -25,18 +25,37 @@ function askUserForBookInfo() {
   addBookToLibrary(book);
 }
 
+let testFirst = new Book("anon", "new book", 49, true);
+
+let testSecond = new Book ("newauthor", "second", 6789, false);
+
+addBookToLibrary(testFirst);
+addBookToLibrary(testSecond);
 
 
 // function getBook() {
 //   let current_book = {};
 // }
 
-// function displayBook() {
+function generateBookHTML(book) {
   // will take current book
-  // generate a tr and attach it to table as last child
+  // generate a tr and 
+  let row = document.createElement('tr');
   // generate a td for each property value (loop for .. in ..)
-// }
+  for (let prop in book){
+    let column = document.createElement('td');
+    column.textContent = book[prop];
+    row.appendChild(column);
+    }
+  return row
+  // attach it to table as last child
+}
 
-// function displayLibrary() {
-  // checkBooks();
-// }
+function displayLibrary() {
+  let containerTable = document.querySelector('.library');
+  let eachBook = myLibrary.forEach (book => {
+    generateBookHTML(book)
+    containerTable.appendChild(eachBook)
+  });
+  
+}
